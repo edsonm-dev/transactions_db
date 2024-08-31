3cea30f24b14a21ed5d7d19fb98a0f43f12ea295
Edson Jorge Matsimbe <m.edsonjorge@gmail.com>
2024-08-31 14:02:43 +0200

    added db schemas and redesigned database

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,106 +1,160 @@
-
-
-
 CREATE DATABASE Test;
+GO
 
-
+USE Test;
+GO
+CREATE SCHEMA Fact;
+GO
+CREATE SCHEMA Dimension;
+GO
 --adding Category table
-
-
-CREATE TABLE test.dbo.category (
-	CategoryID INT PRIMARY KEY NOT NULL,
-	CategoryName NVARCHAR(50) NOT NULL,
-
-);
-
-
-
-BULK INSERT test.dbo.category
-FROM 'D:\full_path\categories.csv'
-WITH (
-	FIELDTERMINATOR='|',
-	FIELDQUOTE='"',
-	FORMAT='CSV',
-	FIRSTROW=2,
-	ROWTERMINATOR='0x0A',
-	CODEPAGE = '65001'
+CREATE TABLE
+	Dimension.Category (
+		ID INT NOT NULL,
+		CategoryName NVARCHAR(50) NOT NULL,
+		CONSTRAINT PK_CATEGORY PRIMARY KEY (ID)
 	);
 
+GO
+BULK INSERT Dimension.Category
+FROM
+	'D:\fullpath\Categories.csv'
+WITH
+	(
+		FIELDTERMINATOR = '|',
+		FIELDQUOTE = '"',
+		FORMAT = 'CSV',
+		FIRSTROW = 2,
+		ROWTERMINATOR = '0x0A',
+		CODEPAGE = '65001'
+	);
 
-
+GO
 --adding subcategories table
-
-CREATE TABLE test.dbo.subcategory (
-	SubCategoryID INT PRIMARY KEY NOT NULL,
-	SubCategoryName NVARCHAR(50) NOT NULL,
-	SubCategoryType NVARCHAR(50) NOT NULL,
-	CategoryID INT NOT NULL FOREIGN KEY REFERENCES category(CategoryID) ON UPDATE CASCADE --Cascade ensures that if i update a CategoryID in the category table it gets updated here as well
-	
-);
-
-BULK INSERT test.dbo.subcategory
-FROM 'D:\full_path\subcategories.csv'
-WITH (
-	FIELDTERMINATOR='|',
-	FIELDQUOTE='"',
-	FORMAT='CSV',
-	FIRSTROW=2,
-	ROWTERMINATOR='0x0A',
-	CODEPAGE = '65001'
+CREATE TABLE
+	Dimension.Subcategory (
+		ID INT NOT NULL,
+		SubCategoryName NVARCHAR(50) NOT NULL,
+		SubCategoryType NVARCHAR(50) NOT NULL,
+		CategoryID INT NOT NULL,
+		CONSTRAINT PK_SUBCATEGORY PRIMARY KEY (ID),
+		CONSTRAINT FK_SUBCATEGORY_CATEGORY FOREIGN KEY (CategoryID) REFERENCES Dimension.Category (ID)
 	);
 
+GO
+
+BULK INSERT Dimension.Subcategory
+FROM
+	'D:\fullpath\Subcategories.csv'
+WITH
+	(
+		FIELDTERMINATOR = '|',
+		FIELDQUOTE = '"',
+		FORMAT = 'CSV',
+		FIRSTROW = 2,
+		ROWTERMINATOR = '0x0A',
+		CODEPAGE = '65001'
+	);
+
+GO
 --adding expressions table
-
-CREATE TABLE test.dbo.expressions (
-	SubCategoryID INT NOT NULL FOREIGN KEY REFERENCES subcategory(SubCategoryID) ON UPDATE CASCADE, --If i delete a category from subcategory table then it will be updated here as well
-	Expression NVARCHAR(50) NOT NULL PRIMARY KEY
-
-);
-
-
-
-BULK INSERT test.dbo.expressions
-FROM 'D:\full_path\expressions.csv'
-WITH (
-	FIELDTERMINATOR='|',
-	FIELDQUOTE='"',
-	FORMAT='CSV',
-	FIRSTROW=2,
-	ROWTERMINATOR='0x0A',
-	CODEPAGE = '65001'
+CREATE TABLE
+	Dimension.Expressions (
+		SubCategoryID INT NOT NULL,
+		Expression NVARCHAR(50) UNIQUE NOT NULL,
+		CONSTRAINT FK_EXPRESSIONS_SUBCATEGORY FOREIGN KEY (SubCategoryID) REFERENCES Dimension.Subcategory (ID),
+		CONSTRAINT PK_EXPRESSIONS PRIMARY KEY (Expression)
 	);
 
+GO
 
---Adding transactions table from file
-
-
-CREATE TABLE Test.dbo.transactions (
-	
-	[OWNER] NVARCHAR(10),
-	[DATE] DATE,
-	SubCategoryID INT FOREIGN KEY REFERENCES subcategory(SubCategoryID)
-		ON DELETE SET NULL
-		ON UPDATE CASCADE,
-	[Description] NVARCHAR(200),
-	Amount DECIMAL(10,2)
-
-);
-
-
-
-
-
-
-BULK INSERT Test.dbo.transactions
-FROM 'D:\full_path\transactions.csv'
-WITH (
-	FIELDTERMINATOR=',',
-	FIELDQUOTE='"',
-	FORMAT='CSV',
-	FIRSTROW=2,
-	ROWTERMINATOR='0x0A',
-	CODEPAGE = '65001'
-	
-	
+BULK INSERT Dimension.Expressions
+FROM
+	'D:\fullpath\Expressions.csv'
+WITH
+	(
+		FIELDTERMINATOR = '|',
+		FIELDQUOTE = '"',
+		FORMAT = 'CSV',
+		FIRSTROW = 2,
+		ROWTERMINATOR = '0x0A',
+		CODEPAGE = '65001'
 	);
 
+GO
+/* -these tables will be populated with ssis, working on it
+CREATE TABLE Dimension.[Owner](
+ID nvarchar(10),
+[Name] nvarchar(20),
+
+CONSTRAINT PK_OWNER PRIMARY KEY (ID)
+);
+GO
+
+CREATE TABLE Dimension.[Date](
+DateKey varchar(20),
+
+
+CONSTRAINT PK_DATE PRIMARY KEY (Datekey)
+);
+GO*/
+--Adding all data table
+CREATE TABLE
+	Fact.Transactions (
+		TransactionID INT IDENTITY(1, 1),
+		OwnerID NVARCHAR(10),
+		Datekey VARCHAR(20),
+		SubCategoryID INT,
+		[Description] NVARCHAR(200),
+		Amount DECIMAL(10, 2),
+		CONSTRAINT FK_TRANSACTIONS_SUBCATEGORY FOREIGN KEY (SubCategoryID) REFERENCES Dimension.Subcategory (ID),
+		/*CONSTRAINT FK_TRANSACTIONS_DATE  FOREIGN KEY (Datekey) REFERENCES Dimension.[Date] (Datekey),
+		CONSTRAINT FK_TRANSACTIONS_OWNER  FOREIGN KEY (OwnerID) REFERENCES Dimension.[Owner] (ID),*/
+		CONSTRAINT PK_TRANSACTIONS PRIMARY KEY (TransactionID)
+	);
+
+GO
+
+CREATE TABLE
+	Transactions_Staging (
+		OwnerID NVARCHAR(10),
+		Datekey VARCHAR(20),
+		SubCategoryID INT,
+		[Description] NVARCHAR(200),
+		Amount DECIMAL(10, 2),
+	);
+
+GO
+
+BULK INSERT Transactions_Staging
+FROM
+	'D:\fullpath\all_data.csv'
+WITH
+	(
+		FIELDTERMINATOR = ',',
+		FIELDQUOTE = '"',
+		FORMAT = 'CSV',
+		FIRSTROW = 2,
+		ROWTERMINATOR = '0x0A',
+		CODEPAGE = '65001'
+	);
+
+GO
+INSERT INTO
+	Fact.Transactions (
+		OwnerID,
+		Datekey,
+		SubCategoryID,
+		[Description],
+		Amount
+	)
+SELECT
+	OwnerID,
+	Datekey,
+	SubCategoryID,
+	[Description],
+	Amount
+FROM
+	Transactions_Staging;
+
+GO
--- 02_stored_procedures_triggers.sql
+++ 02_stored_procedures_triggers.sql
@@ -2,90 +2,109 @@
 --STORED PROCEDURE TO INSERT A NEW TRANSACTION INTO THE TRANSACTION TABLE WITH FOR EXAMPLE A PYTHON CODE
 --AFTER THE INSERT, THE CODE TRIES TO FIND THE SUBCATEGORY ID BASED ON THE EXPRESSION TABLE
 
-CREATE PROCEDURE InsertUpdateTransactions
-(@Owner  NVARCHAR(50),
-@Date  DATE,
-@Description   NVARCHAR(200),
-@Amount DECIMAL(20,2)
-)
-AS
+USE Test;
 
-BEGIN
+GO 
 
-	SET NOCOUNT ON;
+CREATE PROCEDURE InsertUpdateTransactions (
+	@Owner NVARCHAR(50),
+	@Date DATE,
+	@Description NVARCHAR(200),
+	@Amount DECIMAL(20, 2)
+) 
+AS BEGIN
 
-	INSERT INTO Test.dbo.transactions ([OWNER],[DATE],[Description],[Amount])
-	VALUES (@Owner, @Date, @Description, @Amount)
+	SET NOCOUNT ON
+	INSERT INTO
+		Fact.Transactions ([OWNERID], [DATEKEY], [Description], [Amount])
+	VALUES
+		(@Owner, @Date, @Description, @Amount) DECLARE @NewSubID INT
 
-
-	DECLARE @NewSubID INT;
-
-	SELECT TOP 1 @NewSubID =  e.SubCategoryID
-	FROM Test.dbo.expressions e
-	WHERE @Description LIKE '%'+ e.Expression + '%' 
-	ORDER BY len(e.expression) DESC
-
-	UPDATE Test.dbo.transactions 
-	SET SUBCATEGORYID = @NewSubId
-	WHERE TransactionID=SCOPE_IDENTITY();
+	SELECT
+		TOP 1 @NewSubID = e.SubCategoryID
+	FROM
+		Dimension.Expressions e
+	WHERE
+		@Description LIKE '%' + e.Expression + '%'
+	ORDER BY
+		LEN(e.expression) DESC
+	UPDATE Fact.Transactions
+	SET
+		SUBCATEGORYID = @NewSubId
+	WHERE
+		TransactionID = SCOPE_IDENTITY()
 
 END;
+
+
+GO
 
 --CREATING THE SAME AUTOMATION WITH TRIGGER FOR PRACTICE
 --IF THERE IS A NEW TRANSACTION INSERTED IN THE TRANSACTIONS TABLE, IT TRIES TO FIND THE SUBCATEGORY ID IN THE EXPRESSION TABLE
 
-CREATE TRIGGER SubCategoryUpdate ON
-Test.dbo.transactions AFTER INSERT
+CREATE TRIGGER SubCategoryUpdate 
+ON Fact.Transactions AFTER INSERT
 
-AS
-
-BEGIN
+AS BEGIN 
 	
-	DECLARE @NewSubID INT;
+	DECLARE @NewSubID INT
 
-	SELECT TOP 1 @NewSubID=e.SubCategoryID
-	FROM Test.dbo.expressions e
-	INNER JOIN inserted i ON i.[Description] LIKE '%' + e.Expression + '%'
-	ORDER BY len(e.Expression) DESC;
+	SELECT
+		TOP 1 @NewSubID = e.SubCategoryID
+	FROM
+		Dimension.Expressions e
+		INNER JOIN inserted i ON i.[Description] LIKE '%' + e.Expression + '%'
+	ORDER BY
+		LEN(e.Expression) DESC
 
-	UPDATE Test.dbo.transactions 
-	SET SubCategoryID = @NewSubID
-	FROM Test.dbo.transactions t
-	INNER JOIN inserted i ON i.TransactionID = t.TransactionID
+	UPDATE Fact.Transactions
+	SET
+		SubCategoryID = @NewSubID
+	FROM
+		Fact.Transactions t
+		INNER JOIN inserted i ON i.TransactionID = t.TransactionID 
 
-END
+END;
+
+GO
 
 --FUNCTION FOR UPDATING ALL ROWS' SUBCATEGORY
 --USED BY THE PROCEDURE BELOW
 --IT CHECKS IF THERE IS AN EXPRESSION IN THE EXPRESSIONS TABLE THAT IS CONTAINED IN THE DESCRIPTION AND RETURNS THE ID
 --IF THERE ARE MULTIPLE MATCHES IT WILL PRIORITIZE THE ONE THAT IS THE LONGEST
 
-CREATE FUNCTION FindSubCategoryID (@Description NVARCHAR(200))
-RETURNS INT
+CREATE FUNCTION FindSubCategoryID (
+	@Description NVARCHAR(200)) 
+RETURNS INT 
+AS BEGIN 
 
-AS BEGIN
+	DECLARE @NewSubID INT
 
-	DECLARE @NewSubID INT;
-
-	SELECT TOP 1 @NewSubID =  e.SubCategoryID
-	FROM Test.dbo.expressions e
-	WHERE @Description LIKE '%'+ e.Expression + '%' 
-	ORDER BY len(e.expression) DESC
-
-	RETURN @NewSubID
+	SELECT
+		TOP 1 @NewSubID = e.SubCategoryID
+	FROM
+		Dimension.Expressions e
+	WHERE
+		@Description LIKE '%' + e.Expression + '%'
+	ORDER BY
+		LEN(e.expression) DESC RETURN @NewSubID 
 
 END;
+
+GO
 
 --PROCEDURE IF WE WANT TO UPDATE ALL SUBCAT IDS
 --THIS USES THE FindSubCategoryID FUNCTION CREATED ABOVE
 
-CREATE PROCEDURE UpdateAllSubCatID
-AS
-BEGIN
+CREATE PROCEDURE UpdateAllSubCatID 
+AS BEGIN
 
-	SET NOCOUNT ON;
+	SET NOCOUNT ON
 
-	UPDATE Test.dbo.transactions 
-	SET  SUBCATEGORYID = Test.dbo.findsubcategoryID([Description])
+	UPDATE Fact.Transactions
+	SET
+		SUBCATEGORYID = dbo.FindSubCategoryID ([Description]) 
 
-END
+END;
+
+GO
